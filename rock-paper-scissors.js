353de52792e12a65bb062e1922959387a35d66f9
Andromu <andresrodriguez3d@gmail.com>
2024-08-10 09:54:54 +0100

    Add getComputerChoice function and getHumanChoice function.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,25 @@
-console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice () {
+    let number = Math.round(Math.random() * 2 );
+    switch (number) {
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        default:
+            return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
+
+function getHumanChoice (){
+    let option = prompt("Choose between Rock, Paper or Scissors.");
+    return option.toLowerCase();
+}
+
+console.log(getHumanChoice());
